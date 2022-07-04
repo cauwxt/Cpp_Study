2a48bd4a1b0021b4077d24b6e1216cdbf94b987a
Xiaotao.Wang <cauwxt@qq.com>
2022-07-04 21:35:33 +0800

    +1

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
-int main (){
-    cout << "HelloWorld!"<< endl;
+int main(int argc, char const *argv[])
+{
+    if (argc != 1)
+    {
+        for (size_t i = 0; i < argc; i++)
+        {
+            cout << "parm" << i << ":" << argv[i] << endl;
+        }
+        
+    } 
+    else{
+        cout << "参数缺失" << endl;
+    }
+    return 0;
 }
